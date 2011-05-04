80b94f10e3a3ab824cf22c5e87c8d003f63b1359
Luis Pereira <luis.mdc.pereira@hotmail.com>
2011-05-04 22:32:42 +0100

    Parte II - pergunta 4

--- Sessao1 - CLI Part1/testep.cs
+++ Sessao1 - CLI Part1/testep.cs
@@ -1,11 +1,21 @@
-﻿
+﻿using System;
+
 namespace Sessao1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-          
+          Ponto p1 = new Ponto(1,1);
+		  Ponto p2 = new Ponto(2,2);
+		  
+		  int distancia = p2.Distance(p1);
+		  
+		  Console.WriteLine(p1.ToString());
+		  Console.WriteLine(p2.ToString());
+		  Console.WriteLine("Distancia: {0}", distancia);
+		  
+		  Console.WriteLine("CompareTo: {0}", p1.CompareTo(p2));
         }
     }
 }
